6f0e54d0c946cb0ed2fdd530025642abf14e6dd7
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2022-12-22 06:45:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ st.write('You selected:', option)
 def Driver():
     chrome_options = webdriver.ChromeOptions()
 #     output_path = os.path.join(os.getcwd(),'output')
-    download_excel_prefs = {"download.default_directory" : output_path}
+#     download_excel_prefs = {"download.default_directory" : output_path}
     chrome_options.add_experimental_option("prefs",download_excel_prefs)   
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument('--headless')
