5e410f971abee4c2f58fe19b7b9d46e638a33ad5
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2022-12-22 08:05:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
